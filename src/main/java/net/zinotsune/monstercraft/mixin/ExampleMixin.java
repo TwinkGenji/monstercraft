7677f878f1d12efe11294a9d8d032ac68229715d
MandL27 <mandl27g@gmail.com>
2022-06-24 19:05:58 -0400

    Add GeckoLib dependency and rename packages

--- src/main/java/net/zinotsune/monstercraft/mixin/ExampleMixin.java
+++ src/main/java/net/zinotsune/monstercraft/mixin/ExampleMixin.java
@@ -1,7 +1,8 @@
-package net.fabricmc.example.mixin;
+package net.zinotsune.monstercraft.mixin;
 
-import net.fabricmc.example.ExampleMod;
 import net.minecraft.client.gui.screen.TitleScreen;
+import net.zinotsune.monstercraft.MonsterCraftMod;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,6 +12,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		MonsterCraftMod.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
